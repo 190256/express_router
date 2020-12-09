368bbfed2dcd4ab6828a2ea11437102916fc3a64
190256 <190256hr@yse-c.net>
2020-12-09 10:21:12 +0900

    Second

---
+++ server.js
@@ -0,0 +1,25 @@
+// モジュール読み込み
+var express = require("express")
+
+// ExpressのRouter 作成
+var router = express.Router()
+
+// サーバ作成
+var app = express()
+
+// ミドルウェア router
+app.use('/', router)
+
+// ルーティング
+router.get('/', (req, res) => {
+    res.send('Hello Express Router!!')
+});
+
+router.get('/message/list', (req, res) => {
+    res.send('Message list page')
+});
+
+// サーバ待機
+app.listen(3000)
+
+console.log('Listen: http://localhost:3000')
