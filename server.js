8e54fe0079c948406386d3dd2d654b1a57dee603
190256 <190256hr@yse-c.net>
2020-12-09 10:54:09 +0900

    Third

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 // モジュール読み込み
-var express = require("express")
+var express = require("express");
 
 // ExpressのRouter 作成
-var router = express.Router()
+var router = express.Router();
 
 // サーバ作成
-var app = express()
+var app = express();
 
 // ミドルウェア router
-app.use('/', router)
+app.use("/", router);
 
 // ルーティング
-router.get('/', (req, res) => {
-    res.send('Hello Express Router!!')
+router.get("/", (req, res) => {
+    res.send("Hello Express Router!!");
 });
 
-router.get('/message/list', (req, res) => {
-    res.send('Message list page')
+router.get("/profile", (req, res) => {
+    res.send("Tokyo Taro");
 });
 
 // サーバ待機
-app.listen(3000)
+app.listen(3000);
 
-console.log('Listen: http://localhost:3000')
+console.log("Listen: http://localhost:3000");
